9bc2d8c2fc32604c7b55fb984ff7c75ce283edd1
federico-ricci <fricci748@gmail.com>
2023-12-06 16:29:03 +0100

    first commit

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+// const pageForm = document.getElementById('mainForm')
+
+// pageForm.addEventListener('submit', function(e)); {
+
+// e.preventDefault()
+
+// const agendaInputField = document.getElementById('Task')
+
+
+// }
+
+const submit = function(){
+    alert ('test!')
+}
+// non riesco a fare un test, il tasto non funziona non capisco se è un problema mio di sintassi o altro....
+
